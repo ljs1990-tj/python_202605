c98cf676ee7ec5a04e2f253c913b1a2c7dc64119
test <139727181+ljs1990-tj@users.noreply.github.com>
2026-05-08 17:17:07 +0900

    2026.05.08

--- day2/14_문제풀이.py
+++ day2/14_문제풀이.py
@@ -11,3 +11,39 @@
 # 검색은 이름으로 검색 후 없으면 '없는 학생' 출력
     # 있으면 학생의 이름, 학과, 주소, 번호 출력 
 # 종료는 프로그램 종료
+
+students = {}
+while True :
+    menu = input("[ (1)등록 (2)수정 (3)삭제 (4)검색 (5)종료 ] : ")
+    if menu == "1" :
+        name = input("이름 : ")
+        dept = input("학과 : ")
+        addr = input("주소 : ")
+        phone = input("번호 : ")
+        students[name] = {"addr" : addr, "dept" : dept, "phone" : phone}
+    elif menu == "2" :
+        name = input("이름 : ")
+        if name in students :
+            dept = input("학과 : ")
+            addr = input("주소 : ")
+            phone = input("번호 : ")
+            students[name] = {"addr" : addr, "dept" : dept, "phone" : phone}
+        else :
+            print("없는 학생")
+    elif menu == "3" :
+        name = input("이름 : ")
+        if name in students :
+            students.pop(name)
+            print("삭제되었습니다.")
+        else :
+            print("없는 학생")
+    elif menu == "4" :
+        name = input("이름 : ")
+        if name in students :
+            student = students[name]
+            print(f"이름 : {name}, 학과 : {student["dept"]}, 주소 : {student["addr"]}, 번호 : {student["phone"]}")
+        else :
+            print("없는 학생")
+    else :
+        print("종료되었습니다.")
+        break
